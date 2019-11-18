383392228726e775d46c93d2d46d6aa0b23ee43b
brian <bchou515@gmail.com>
2019-11-18 17:12:30 -0500

    create class

---
+++ src/main/java/com/chapter/one/CheckPermutation.java
@@ -0,0 +1,13 @@
+package com.chapter.one;
+
+public class CheckPermutation {
+    /*
+        Given 2 strings, write a method to decide if one is a permutation 
+        of the other.
+    */
+
+    public static boolean isPermutation(String a, String b) {
+        return false;
+    }
+    
+}
