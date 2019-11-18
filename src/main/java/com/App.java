8b134f1dd3ce30c29685e0d64f1750f3b4a05e4c
brian <bchou515@gmail.com>
2019-11-18 16:36:40 -0500

    clean prior code in App.java

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,20 +1,7 @@
 package com;
 
-import java.util.Arrays;
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main( String[] args ) {
-        System.out.println(calculate(new int[]{3,4,-4,8,11,-1,6}, 10));
+        System.out.println("Hello world");
     }
-
-    public static int calculate(int[] array, int targetSum) {
-        Arrays.sort(array);
-        System.out.println(String.format("array: %s | targetSum: %d", Arrays.toString(array), targetSum));
-        return 0;
-    }
-
-
 }
