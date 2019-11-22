dffaddda81ce91d6336f43dc4f149109ff51e7bd
brian <bchou515@gmail.com>
2019-11-21 20:23:25 -0500

    extra test and readme check off

--- src/test/java/com/chapter/two/ReturnKthToLastTest.java
+++ src/test/java/com/chapter/two/ReturnKthToLastTest.java
@@ -30,4 +30,20 @@ public class ReturnKthToLastTest {
         Optional<LLNode> solution = ReturnKthToLast.returnKthToLast(null, 2);
         assertEquals(Optional.empty(), solution);
     }
+
+    @Test
+    public void returnKthToLast_WithKEqualingN_ShouldReturnEmpty() {
+        LLNode head = new LLNode(0);
+        LLNode node1 = new LLNode(7);
+        LLNode node2 = new LLNode(8);
+        LLNode node3 = new LLNode(9);
+        LLNode node4 = new LLNode(11);
+
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        Optional<LLNode> solution = ReturnKthToLast.returnKthToLast(head, 5);
+        assertEquals(Optional.empty(), solution);
+    }
 }
