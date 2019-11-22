f1a82cfa50315b1357cdc2bc0a25fa9ec568321e
brian <bchou515@gmail.com>
2019-11-21 20:17:50 -0500

    test

--- src/main/java/com/chapter/two/ReturnKthToLast.java
+++ src/main/java/com/chapter/two/ReturnKthToLast.java
@@ -1,28 +1,28 @@
 package com.chapter.two;
 
+import java.util.Optional;
+
 public class ReturnKthToLast {
 
-    public LLNode returnKthToLast(LLNode head, int k) {
-        // check if head is null
+    public static Optional<LLNode> returnKthToLast(LLNode head, int k) {
         if (head == null) {
-            return null;
+            return Optional.empty();
         }
 
         LLNode cursor = head;
         while (k > 0) {
             if (cursor.next == null)
-                return null;
+                return Optional.empty();
             cursor = cursor.next;
             k--;
         }
 
         LLNode answer = head;
-        while (cursor != null) {
+        while (cursor.next != null) {
             cursor = cursor.next;
             answer = answer.next;
         }
-        return answer;
+        return Optional.of(answer);
     }
-
 
 }
---
+++ src/test/java/com/chapter/two/ReturnKthToLastTest.java
@@ -0,0 +1,33 @@
+package com.chapter.two;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Optional;
+
+import org.junit.Test;
+
+public class ReturnKthToLastTest {
+    @Test
+    public void returnKthToLastTest_WithNormalList_ShouldReturnEight() {
+        LLNode head = new LLNode(0);
+        LLNode node1 = new LLNode(7);
+        LLNode node2 = new LLNode(8);
+        LLNode node3 = new LLNode(9);
+        LLNode node4 = new LLNode(11);
+
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        Optional<LLNode> solution = ReturnKthToLast.returnKthToLast(head, 2);
+
+        assertEquals(8, solution.get().value);
+    }
+
+    @Test
+    public void returnKthToLastTest_WithNull_ShouldReturnEmpty() {
+        Optional<LLNode> solution = ReturnKthToLast.returnKthToLast(null, 2);
+        assertEquals(Optional.empty(), solution);
+    }
+}
