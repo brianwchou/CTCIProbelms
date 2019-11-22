2688298992a018ba7b477377e39fa2843c4a5b91
brian <bchou515@gmail.com>
2019-11-21 21:55:56 -0500

    checkpoint

--- src/main/java/com/chapter/two/RemoveDups.java
+++ src/main/java/com/chapter/two/RemoveDups.java
@@ -1,5 +1,19 @@
 package com.chapter.two;
 
 public class RemoveDups {
-    
+
+    public static void removeDups(LLNode head) {
+        while (head != null) {
+            int value = head.value;
+            System.out.printf("%d, %b%n", value, head.next != null);
+            LLNode cursor = head;
+            while (cursor != null && cursor.next != null) {
+                if (cursor.next.value == value) {
+                    cursor.next = cursor.next.next;
+                }
+                cursor = cursor.next;
+            }
+            head = head.next;
+        }
+    }
 }
--- src/test/java/com/chapter/two/RemoveDupsTest.java
+++ src/test/java/com/chapter/two/RemoveDupsTest.java
@@ -1,5 +1,38 @@
 package com.chapter.two;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
 public class RemoveDupsTest {
     
+    @Test
+    public void removeDups() {
+        LLNode head = new LLNode(8);
+        LLNode node1 = new LLNode(0);
+        LLNode node2 = new LLNode(7);
+        LLNode node3 = new LLNode(8);
+        LLNode node4 = new LLNode(9);
+        LLNode node5 = new LLNode(8);
+        LLNode node6 = new LLNode(8);
+        LLNode node7 = new LLNode(11);
+        //LLNode node8 = new LLNode(8);
+
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+        //node7.next = node8;
+
+        RemoveDups.removeDups(head);
+        
+        ArrayList<Integer> check = new ArrayList<Integer>(Arrays.asList(8,0,7,9,11));
+        assertTrue(check.equals(head.toArrayList()));
+    }
 }
