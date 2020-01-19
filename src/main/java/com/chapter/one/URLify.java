ec349b644666c431d298d17dd13565b694d1d831
brian <bchou515@gmail.com>
2020-01-19 14:33:32 -0500

    starting files

---
+++ src/main/java/com/chapter/one/URLify.java
@@ -0,0 +1,27 @@
+package com.chapter.one;
+
+/*
+    1.3 URLify: Write a method to replace all spaces in a string with '%20'. You 
+    may assume that the string has suffient space and the true length is given and
+    that you are given the "true length of the string". (Note if implmenting this 
+    in Java, please use a character array so that you can perform this operation in
+    place.
+    EXAMPLE
+    Input:  "Mr John Smith    ", 13
+    Output: "Mr%20John%20Smith"
+
+*/
+public class URLify {
+
+    /*
+        Assume that theres enough space within the char array
+        to solve the problem
+    */ 
+    public static void createAsURL(char[] string) {
+        // start from the back and sift the words to the back while inserting
+        // %20 where needed
+
+        
+
+    }  
+}
