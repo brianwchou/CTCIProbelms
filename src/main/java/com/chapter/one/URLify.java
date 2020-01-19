a08273ef1f950abaffa66858c683163a38988f3e
brian <bchou515@gmail.com>
2020-01-19 15:04:50 -0500

    test

--- src/main/java/com/chapter/one/URLify.java
+++ src/main/java/com/chapter/one/URLify.java
@@ -12,16 +12,40 @@ package com.chapter.one;
 
 */
 public class URLify {
-
     /*
         Assume that theres enough space within the char array
         to solve the problem
+
+        runtime is O(N) since we only run through the array once
     */ 
     public static void createAsURL(char[] string) {
         // start from the back and sift the words to the back while inserting
         // %20 where needed
 
-        
+        // starting from the back find the first instance of nonwhitespace
+        // if there isnt any whietspace in the back we cannot run this algorithm
 
+        int cursor = string.length - 1;
+        while (cursor > -1 && Character.isWhitespace(string[cursor])) {
+            cursor--;
+        }
+
+        // if there isnt any whitespace we would ideally throw an exception here
+
+        // now that we have the location of non whitespace
+        // we can proceed with shifting values
+
+        int lastInsert = string.length - 1;
+        while (cursor > -1 && lastInsert > -1) {
+            if (Character.isWhitespace(string[cursor])) {
+                string[lastInsert--] = '0';
+                string[lastInsert--] = '2';
+                string[lastInsert--] = '%';
+            } else { // move character to lastInsert
+                string[lastInsert] = string[cursor];
+                lastInsert--;
+            }
+            cursor--;
+        }
     }  
 }
--- src/test/java/com/chapter/one/URLifyTest.java
+++ src/test/java/com/chapter/one/URLifyTest.java
@@ -1,5 +1,18 @@
 package com.chapter.one;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class URLifyTest {
-    
+
+    @Test
+    public void createAsURL_withNormalInput() {
+        char[] input = "Mr John Smith    ".toCharArray();
+        char[] answer = "Mr%20John%20Smith".toCharArray();
+        
+        URLify.createAsURL(input);
+
+        assertArrayEquals(answer, input);
+    }
 }
