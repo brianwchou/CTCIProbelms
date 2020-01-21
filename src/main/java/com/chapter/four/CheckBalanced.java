a3a2020d5211b7bd6adefc8935c2a89533d424b9
brian <bchou515@gmail.com>
2020-01-20 20:44:57 -0500

    naive implementstion with analysis

--- src/main/java/com/chapter/four/CheckBalanced.java
+++ src/main/java/com/chapter/four/CheckBalanced.java
@@ -1,5 +1,7 @@
 package com.chapter.four;
 
+import com.chapter.four.util.BTNode;
+
 /*
     4.4 Check Balanced: implement a function to check if a binary tree is balanced. For
     the purposes of this question, a balanced tree is defined such that the heights of 
@@ -8,4 +10,34 @@ package com.chapter.four;
 
 public class CheckBalanced {
     
+    /*
+        this method runs at O(N^2) time since the count is caluclated at
+        very node which requires you to 
+    */
+    public static boolean isTreeBalancedNaive(BTNode root) {
+        if (root == null) {
+            return true;
+        }
+        
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return false;
+        }
+
+        return isTreeBalancedNaive(root.left) && isTreeBalancedNaive(root.right);
+    }
+
+    private static int getHeight(BTNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+
+    }
 }
--- src/test/java/com/chapter/four/CheckBalancedTest.java
+++ src/test/java/com/chapter/four/CheckBalancedTest.java
@@ -1,5 +1,27 @@
 package com.chapter.four;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.chapter.four.util.BTNode;
+
+import org.junit.jupiter.api.Test;
+
 public class CheckBalancedTest {
     
+
+    @Test
+    void checkBalanced_withNormalTree() {
+        // arrage
+        BTNode root = new BTNode(1);
+        root.left = new BTNode(2);
+        root.right = new BTNode(3);
+        root.left.left = new BTNode(4);
+        root.left.right = new BTNode(5);
+        root.left.right.left = new BTNode(6);
+        root.right.right = new BTNode(7);
+
+        assertTrue(CheckBalanced.isTreeBalancedNaive(root), "");
+        
+        
+    }
 }
